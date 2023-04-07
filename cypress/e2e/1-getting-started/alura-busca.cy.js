ec554294f68f0c1211159becd42cd38013c72729
marcosoliveirazup <76001588+marcosoliveirazup@users.noreply.github.com>
2023-04-07 11:43:32 -0300

    aprendendo sobre selecionar objetos

--- cypress/e2e/1-getting-started/alura-busca.cy.js
+++ cypress/e2e/1-getting-started/alura-busca.cy.js
@@ -6,7 +6,7 @@ describe('alura busca cursos',() => {
     it('buscar curso de java', () => {
         cy.get('#header-barraBusca-form-campoBusca').type('java');
         cy.get('.header-barraBusca-form-submit').click();
-        cy.get(':nth-child(2) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
-            .should('have.text','Formação Aprenda Java com Orientação a Objetos');
+        cy.get('h4.busca-resultado-nome')
+            .should('contain.text','Formação Aprenda Java com Orientação a Objetos');
     })
 })
