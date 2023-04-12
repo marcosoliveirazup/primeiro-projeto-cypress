62ee82689d95af8a2781292326af251473ffebf6
marcosoliveirazup <76001588+marcosoliveirazup@users.noreply.github.com>
2023-04-11 23:02:29 -0300

    Alteração
    Exclusão de testes de exemplo e inclusão do relatório

--- cypress/e2e/3-ALURAPIC/alura-pic.cy.js
+++ cypress/e2e/3-ALURAPIC/alura-pic.cy.js
@@ -12,4 +12,30 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage','User name is required!').should('be.visible');
         cy.contains('ap-vmessage','Password is required!').should('be.visible');
     })
+
+    it('verifca mensagem de email invalido', () => {
+        cy.contains('a','Register now').click();
+        cy.contains('button','Register').click();
+        cy.get('input[formcontrolname="email"]').type('jaqueline')
+        cy.contains('ap-vmessage','Invalid e-mail').should('be.visible');
+        
+    })
+
+    it('verifca mensagem de senha com menos de 8 caracteres', () => {
+        cy.contains('a','Register now').click();
+        cy.contains('button','Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button','Register').click();
+        cy.contains('ap-vmessage','Mininum length is 8').should('be.visible');
+        
+    })
+
+    it('verifca mensagem de senha com menos de 8 caracteres', () => {
+        cy.contains('a','Register now').click();
+        cy.contains('button','Register').click();
+        cy.get('input[formcontrolname="userName"]').type('Marcos');
+        cy.contains('button','Register').click();
+        cy.contains('ap-vmessage','Must be lower case').should('be.visible');
+        
+    })
 })
